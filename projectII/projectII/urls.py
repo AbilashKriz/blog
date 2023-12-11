3c3ee82a01ca832db540cdbe7e6bd3351b181606
AbilashKriz <abilaz.r@gmail.com>
2023-12-11 12:01:47 +0530

    images

--- projectII/projectII/urls.py
+++ projectII/projectII/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from djangoPartII import views
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
